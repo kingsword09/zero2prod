ad000aa001f2632c2321f8b614585e9c4c3c16af
Kingsword <kingsword09@gmail.com>
2024-11-22 20:25:30 +0800

    :sparkles: update sql and tests

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -6,6 +6,7 @@ use zero2prod::{
     startup::{build, get_connection_pool, Application},
     telemetry::{get_subscriber, init_subscriber},
 };
+use wiremock::MockServer;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -23,6 +24,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer
 }
 
 impl TestApp {
@@ -40,10 +42,13 @@ impl TestApp {
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
         c.database.database_name = Uuid::new_v4().to_string();
         c.application.port = 0;
+        c.email_client.base_url = email_server.uri();
         c
     };
 
@@ -64,6 +69,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server
     }
 }
 
